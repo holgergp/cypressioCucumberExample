d7a7c2eefa116ce22b15125251d66cf6ed5e5bcb
Holger Grosse-Plankermann <holgergp@gmail.com>
2019-11-28 13:09:23 +0100

    Version bump and fix jest test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {createStore} from 'redux';
+import rootReducer from './reducer';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+    const store = createStore(
+        rootReducer
+    );
+    const div = document.createElement('div');
+    ReactDOM.render(<App store={store}/>, div);
+    ReactDOM.unmountComponentAtNode(div);
 });
